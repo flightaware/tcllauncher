0d186ac75460a98cb05ec389386a156ba8a69f3b
karl <karl@df406022-fe11-11dd-a3ef-1f8437fb274a>
2007-12-29 01:30:33 +0000

    Make it look for ../lib/$programName/main.tcl instead of ../lib/tcllauncher/$programName.tcl.  This may be revisited.

--- tcllauncher.tcl
+++ tcllauncher.tcl
@@ -2,7 +2,7 @@
 # tcllauncher.tcl - tcl code that tcllauncher uses to do its thing
 #
 #
-# $Id: tcllauncher.tcl,v 1.2 2007-10-19 22:33:54 karl Exp $
+# $Id: tcllauncher.tcl,v 1.3 2007-12-29 01:30:33 karl Exp $
 #
 
 namespace eval ::tcllauncher {
@@ -10,12 +10,13 @@ namespace eval ::tcllauncher {
 proc doit {{argv ""}} {
     set prog [info nameofexecutable]
     set path [file split $prog]
+    set shortName [lindex $path end]
 
     #
     # tcllauncher cannot be invoked directly as "tcllauncher" -- it must be
     # aliased to some other name
     #
-    if {[lindex $path end] == "tcllauncher"} {
+    if {$shortName == "tcllauncher"} {
         puts stderr "tcllauncher cannot be invoked as \"tcllauncher\"; it must be copied or linked as some other name"
 	exit 255
     }
@@ -27,10 +28,21 @@ proc doit {{argv ""}} {
     # tcllauncher was found.
     #
 
+    #puts stderr "path '$path', prog '$prog', shortName '$shortName'"
+
     if {[lindex $path end-1] == "bin"} {
-        set path [eval file join [lreplace $path end-1 end-1 lib tcllauncher]].tcl
+	# this version looks for ../lib/tcllauncher/$shortName.tcl`
+	#set path [eval file join [lreplace $path end-1 end-1 lib tcllauncher]]
+
+	# this version looks for ../lib/$shortName/main.tcl
+        set path [eval file join [lreplace $path end-1 end lib $shortName main.tcl]]
     } else {
         set path $prog.tcl
+    }
+
+    if {![file readable $path]} {
+	puts stderr "$shortName: can't read '$path' (tcllauncher)"
+	exit 254
     }
 
     # ok now source in the file we (tcllauncher) figured out is the one
