44335ff915749fb34334232a0918380c763efa53
karl <karl@df406022-fe11-11dd-a3ef-1f8437fb274a>
2008-03-25 22:19:27 +0000

    Add some stuff to make it possible require user and group to be set a certain way.

--- tcllauncher.tcl
+++ tcllauncher.tcl
@@ -2,11 +2,72 @@
 # tcllauncher.tcl - tcl code that tcllauncher uses to do its thing
 #
 #
-# $Id: tcllauncher.tcl,v 1.6 2008-03-18 07:14:45 karl Exp $
+# $Id: tcllauncher.tcl,v 1.7 2008-03-25 22:19:27 karl Exp $
 #
 
 namespace eval ::tcllauncher {
 
+#
+# require_group - require a certain group ID, exit with message to stderr if not
+#
+proc require_group {group} {
+    package require Tclx
+
+    if {[id group] == $group} {
+	return
+    }
+
+    # see if we can set to that group, maybe we're root?
+    if {[catch {id group $group} result] == 1} {
+	puts stderr "requires and can't set to group '$group': $result"
+	exit 254
+    }
+
+    return
+}
+
+#
+# require_user - require a certain user ID, exit with message to stderr if not
+#
+proc require_user {user} {
+    package require Tclx
+
+    if {[id user] == $user} {
+	return
+    }
+
+    # see if we can set to that group, maybe we're root?
+    if {[catch {id user $user} result] == 1} {
+	puts stderr "requires and can't set to user '$user': $result"
+	exit 253
+    }
+
+    return
+}
+
+#
+# require_user_and_group - require the invoker to either be of a certain
+#  user and group or if they're superuser or some kind of equivalent,
+#  force this process to have the specified user (uid) and group (gid)
+#
+require_user_and_group {user group} {
+
+    # try group first because if we're root we might not be after setting
+    # user
+
+    require_group $group
+
+    require_user $user
+}
+
+#
+# this is the code that gets called when Tcl launches because the
+# tcllauncher "version" of Tcl jimmies the command line to make it
+# so.
+#
+# this routine must now figure out what to do to launch the app
+# 
+#
 proc doit {{argv ""}} {
     set prog [info nameofexecutable]
 
